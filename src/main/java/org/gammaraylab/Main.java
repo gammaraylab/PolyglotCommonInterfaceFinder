bd2b1c903fe5e588a078d83048910eae81550203
anadi <anadimitra777@gmail.com>
2024-09-24 15:03:24 +0530

    update test cases

--- src/main/java/org/gammaraylab/Main.java
+++ src/main/java/org/gammaraylab/Main.java
@@ -6,27 +6,14 @@
 
  public class Main {
     public static void main(String[] args) {
-        long n=1000_000L;
-        ComplexNum c1= new ComplexNum(23.4, 453.2);
-        TestCases tc= new TestCases();
-
-//        tc.passingJavaObj(c1);
-//        ArrayList<ComplexNum> l=tc.useJavaObj(n);
-//        System.out.println(l.size());
-//        tc.addComplexLoop(5000);
-        loop();
-
-//        tc.pythonLoop(1000000L);
-//        tc.rubyLoop();
+        System.out.println(loop());
     }
 
-    static void loop(){
+    static long loop(){
         TestCases tc= new TestCases();
-        long i=1000000000000L;
+        long i=10000L;
         long res=0;
-        while(i>0){
-            res+=tc.addAndDivide(i,i*i);
-            i--;
-        }
+        tc.addComplexLoop(i);
+        return res;
     }
  }
